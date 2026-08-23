d7f9c659fd5f84b6e8b9491b1e078168c5fd1d2f
Bhat Nishanth Ganesh <nishanthbhat18@gmail.com>
2026-08-23 21:20:23 +0530

    Time: 3 ms (46.39%), Space: 9.3 MB (45.86%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isPalindromic(string s) {
-        int l=0,r=s.length()-1;
+        string binary="";
+        for(char c:s){
+            int num=c;
+            string bin=bitset<8>(num).to_string();
+            binary+=bin;
+        }
+        int l=0,r=binary.length()-1;
         while(l<=r){
-            int left=s[l];
-            int right=s[r];
-            for(int i=7;i>=0;i--){
-                if(((left>>i)&1) != ((right>>(7-i))&1)){
-                    return false;
-                }
-            }
-            l++;
-            r--;
+            if(binary[l++]!=binary[r--]) return false;
         }
         return true;
     }
