a88c8e7e6a61f24f3755fd1dff48b083583bfc0f
Bhat Nishanth Ganesh <nishanthbhat18@gmail.com>
2026-09-16 09:51:40 +0530

    Time: 42 ms (58.88%), Space: 53.1 MB (34.11%) - LeetHub

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    int numberOfSets(int n, int k) {
+        vector<vector<long long>>dp(n,vector<long long>(k+1));
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        for(int j=1;j<=k;j++){
+            long long sum=0;
+            for(int i=1;i<n;i++){
+                sum=(sum+dp[i-1][j-1])%mod;
+                dp[i][j]=(dp[i-1][j]+sum)%mod;
+            }
+        }
+        return dp[n-1][k];
+    }
+};
