821fb218d5ad6d47b28fa13546c0e15ee903a7ad
Bhat Nishanth Ganesh <nishanthbhat18@gmail.com>
2026-09-26 10:35:16 +0530

    Time: 2 ms (70.34%), Space: 102.3 MB (91%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {
-        int tot=accumulate(nums.begin(),nums.end(),0);
+    int maxSubarrayk(vector<int>&nums,int k){
         int n=nums.size();
-        int mini=INT_MAX;
-        int k=tot-x;
-        int sum=0,l=0;
+        int maxi=INT_MIN,l=0,sum=0;
         for(int r=0;r<n;r++){
             sum+=nums[r];
             while(l<=r && sum>k){
                 sum-=nums[l++];
             }
-            if(sum==k) mini=min(mini,n-(r-l+1));
+            if(sum==k) maxi=max(maxi,r-l+1);
         }
-        return mini==INT_MAX?-1:mini;
+        return maxi==INT_MIN?-1:maxi;
+    }
+    int minOperations(vector<int>& nums, int x) {
+        int n=nums.size();
+        int tot=accumulate(nums.begin(),nums.end(),0);
+        int k=tot-x;
+        int maxi=maxSubarrayk(nums,k);
+        if(maxi==-1) return -1;
+        return n-maxi;
     }
 };
